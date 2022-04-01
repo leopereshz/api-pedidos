db9572546b18ae8c54e97a0eb92ecf6d42ab5ef6
Leonardo Peres <leonardoperes@migrate.info>
2022-03-31 22:24:46 -0300

    Adiciona listagem de itens em um pedido

--- api_pedidos/api.py
+++ api_pedidos/api.py
@@ -1,9 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Request
+from fastapi.responses import JSONResponse
+from uuid import UUID
+from api_pedidos.esquema import Item
+from api_pedidos.excecao import PedidoNaoEncontradoError
+from api_pedidos.excecao import PedidoNaoEncontradoError, FalhaDeComunicacaoError
+from http import HTTPStatus
 
 
 app = FastAPI()
 
 
+def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> list[Item]:
+    pass
+
+
+@app.exception_handler(PedidoNaoEncontradoError)
+def tratar_erro_pedido_nao_encontrado(request: Request, exc: PedidoNaoEncontradoError):
+    return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"message": "Pedido não encontrado"})
+
+
 @app.get("/healthcheck")
 async def healthcheck():
     return {"status": "ok"}
+
+
+@app.get("/orders/{identificacao_do_pedido}/items")
+def listar_itens(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
+    return itens
+
+@app.exception_handler(FalhaDeComunicacaoError)
+def tratar_erro_falha_de_comunicacao(request: Request, exc: FalhaDeComunicacaoError):
+    return JSONResponse(status_code=HTTPStatus.BAD_GATEWAY, content={"message": "Falha de comunicação com o servidor remoto"})
